be0b5376fda7a8de96172b54bc4cc54dd6306555
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-11-09 23:55:09 +0700

    List component

---
+++ components/List.tsx
@@ -0,0 +1,25 @@
+import ButtonCheck from './ButtonCheck';
+
+type Props = {
+  children: string;
+}
+
+export default function List({children} : Props) {
+  return (
+    <li className="flex py-5 pr-5 text-dark-theme-light-grayish-blue border-b last:border-b-0 border-dark-theme-darkest-grayish-blue">
+      <div className="px-6">
+        <ButtonCheck />
+      </div>
+      <div>{children}</div>
+      <button className="flex justify-center items-center ml-auto focus:outline-none">
+        <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18">
+          <path
+            fill="#494C6B"
+            fill-rule="evenodd"
+            d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"
+          />
+        </svg>
+      </button>
+    </li>
+  );
+}
--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,20 +1,14 @@
 import ButtonCheck from './ButtonCheck';
 
+import List from './List';
+
 export default function TodoList() {
 
   return (
     <ul className="bg-dark-theme-very-dark-desaturated-blue rounded-md">
-      <li className="flex py-5 pr-5 text-dark-theme-light-grayish-blue">
-        <div className="px-6">
-          <ButtonCheck />
-        </div>
-        <div>
-          Learn react typescript TDD 
-        </div>
-        <button className="flex justify-center items-center ml-auto">
-          <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
-        </button>
-      </li>
+      <List>Learn react typescript and TDD</List>
+      <List>Lets go</List>
+      <List>Workout boy!</List>
     </ul>
   );
 }
