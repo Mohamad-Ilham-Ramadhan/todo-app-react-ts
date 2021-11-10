e70401411ae01e606dd2629a473e2853123ec0bf
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-11-10 16:30:02 +0700

    ButtonCheck: change <button> to <input checkbox>

--- components/ButtonCheck.tsx
+++ components/ButtonCheck.tsx
@@ -10,15 +10,14 @@ export default function ButtonCheck() {
     setChecked( prev => !prev );
   }
   return (
-    <div className={clsx("rounded-full p-px flex justify-center items-center bg-gradient-to-br hover:from-check-from hover:to-check-to", border)}>
-      <button 
-        className={clsx('rounded-full w-6 h-6 text-center flex justify-center items-center focus:outline-none bg-dark-theme-very-dark-desaturated-blue', bg)}
-        onClick={handleClick}
-      >
-        {checked && 
-          <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9"><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
+    <div className={clsx("rounded-full p-px flex justify-center items-center bg-gradient-to-br hover:from-check-from hover:to-check-to", border)} onClick={handleClick}>
+      <input type="checkbox" 
+        checked={checked} 
+        className={clsx('rounded-full w-6 h-6 text-center flex justify-center items-center focus:outline-none bg-dark-theme-very-dark-desaturated-blue appearance-none', bg)}
+      />  
+      {checked && 
+          <svg xmlns="http://www.w3.org/2000/svg" width="11" height="9" style={{position: 'absolute'}}><path fill="none" stroke="#FFF" strokeWidth="2" d="M1 4.304L3.696 7l6-6"/></svg>
         }
-      </button>  
     </div>
   );
 }
