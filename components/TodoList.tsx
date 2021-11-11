e1b4f8b8d96bc8451876ba26c78321258302ec50
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-11-11 09:55:20 +0700

    filter todos

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -1,6 +1,6 @@
 import ButtonCheck from './ButtonCheck';
 import { useAppSelector, useAppDispatch } from '../redux/hooks';
-import { clearCompleted, setFilter, Filter } from '../redux/reducers/todoListSlice';
+import { clearCompleted, setFilter, Filter, Todo } from '../redux/reducers/todoListSlice';
 import List from './List';
 import clsx from 'clsx';
 
@@ -9,19 +9,28 @@ export default function TodoList() {
   const filter = useAppSelector( state => state.filter );
   const todos = useAppSelector( state => state.todos );
   const labelRemaining = todos.length > 0 ? `${todos.length} items left` : 'nothing todo zzz';
+  let filteredTodos : Todo[];
+
+  if ( filter === 'all') {
+    filteredTodos = todos;
+  } else if ( filter === 'active' ) {
+    filteredTodos = todos.filter( todo => !todo.completed );
+  } else if ( filter === 'completed' ) {
+    filteredTodos = todos.filter( todo => todo.completed );
+  }
 
   function handleFilter(filter: Filter) {
     dispatch(setFilter(filter));
   }
 
-  const btnFilterClassName = 'mx-2 font-bold dark:hover:text-dark-theme-light-grayish-blue-hover hover:text-light-theme-very-dark-grayish-blue';
+  const btnFilterClassName = 'mx-2 font-bold dark:hover:text-dark-theme-light-grayish-blue-hover hover:text-light-theme-very-dark-grayish-blue focus:outline-none';
   const activeClassName = 'text-primary-bright-blue';
   const activeFilterAll = filter === 'all' ? activeClassName : '';
   const activeFilterActive = filter === 'active' ? activeClassName : '';
   const activeFilterCompleted = filter === 'completed' ? activeClassName : '';
   return (
     <ul className="dark:bg-dark-theme-very-dark-desaturated-blue bg-white rounded-md shadow-2xl">
-      {todos.map( todo => (
+      {filteredTodos.map( todo => (
         <List key={todo.id} id={todo.id} completed={todo.completed}>{todo.title}</List>
       ))}
       <li className="flex py-4 pl-6 pr-3 dark:text-dark-theme-very-dark-grayish-blue text-light-theme-dark-grayish-blue text-sm">
