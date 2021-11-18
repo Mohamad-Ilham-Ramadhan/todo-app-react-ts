5342a60710f245b23da21064520eaeabad77fbae
Mohamad Ilham Ramadhan <mohamadilhamramadhan@gmail.com>
2021-11-18 13:07:43 +0700

    mobile design: jarak title dengan top screen

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,8 +9,8 @@ export default function Home() {
   
   return (
     <Provider store={store}>
-    <div className="dark:bg-mobile-dark sm:dark:bg-desktop-dark bg-mobile-light sm:bg-desktop-light bg-no-repeat dark:bg-dark-theme-very-dark-blue bg-light-theme-very-light-gray min-h-screen text-xs sm:text-base"> 
-      <div className="pt-13 sm:w-136 w-80 mx-auto">
+    <div className="dark:bg-mobile-dark sm:dark:bg-desktop-dark bg-mobile-light sm:bg-desktop-light bg-no-repeat bg-contain dark:bg-dark-theme-very-dark-blue bg-light-theme-very-light-gray min-h-screen text-xs sm:text-base"> 
+      <div className="pt-11 sm:pt-13 sm:w-136 w-80 mx-auto">
         <div className="flex text-white mb-10">
           <div className="mr-auto font-bold text-2xl sm:text-4xl" style={{ letterSpacing: '.75rem' }}>TODO</div>
           <div className="flex items-center">
